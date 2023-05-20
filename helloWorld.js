3e958bed06e2afe81c228ca1bbfaa2251ee2eb86
lesego-biz <134061961+lesego-biz@users.noreply.github.com>
2023-05-20 21:20:01 +0200

    Update helloWorld.js
    logged output to console

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,5 @@
 let userInput = prompt("What is your favorite color?");
 console.log("My favorite color is" + userInput);
+let favNum = prompt("what is yor favorite number?");
+console.log("my favorite number is" + favNum);
+
